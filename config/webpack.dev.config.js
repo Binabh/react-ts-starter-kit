01b83dcaef2d36e11058a81f23af0b10b74c8b46
Binabh <binabhdevkota@gmail.com>
2020-12-05 13:21:07 +0545

    config seperated

--- config/webpack.dev.config.js
+++ config/webpack.dev.config.js
@@ -1,32 +1,34 @@
 const path = require('path'),
   webpack = require('webpack'),
   HtmlWebpackPlugin = require('html-webpack-plugin');
+const APP_PATH = path.resolve(__dirname, '../src');
 
 module.exports = {
-  entry: {
-    app: ['./src/index.tsx'],
-    vendor: ['react', 'react-dom'],
-  },
+  entry: APP_PATH,
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'js/[name].bundle.js',
   },
   devtool: 'source-map',
+  devServer: {
+    open: true,
+  },
   resolve: {
-    extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
+    extensions: ['.js', '.json', '.ts', '.tsx'],
   },
   module: {
     rules: [
       {
         test: /\.(ts|tsx)$/,
         loader: 'ts-loader',
+        exclude: /node_modules/,
       },
       { enforce: 'pre', test: /\.js$/, loader: 'source-map-loader' },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, 'public', 'index.html'),
+      template: path.resolve(__dirname, '../public', 'index.html'),
       filename: 'index.html',
       title: 'Hello Program',
       minify: {
