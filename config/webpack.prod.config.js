640a7b39ccba753805b46feb76d9fcd5e46cd8a8
Binabh <binabhdevkota@gmail.com>
2020-12-05 14:35:08 +0545

    build successful

---
+++ config/webpack.prod.config.js
@@ -0,0 +1,48 @@
+const path = require('path'),
+  webpack = require('webpack'),
+  HtmlWebpackPlugin = require('html-webpack-plugin'),
+  ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const APP_PATH = path.resolve(__dirname, '../src');
+
+module.exports = {
+  entry: APP_PATH,
+  output: {
+    path: path.resolve(__dirname, '../dist'),
+    filename: 'js/[name].bundle.js',
+  },
+  devtool: 'source-map',
+  devServer: {
+    open: true,
+  },
+  resolve: {
+    extensions: ['.js', '.json', '.ts', '.tsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      { enforce: 'pre', test: /\.js$/, loader: 'source-map-loader' },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, '../public', 'index.html'),
+      filename: 'index.html',
+      title: 'Hello Program',
+      minify: {
+        collapseWhitespace: true,
+        collapseInlineTagWhitespace: true,
+        minifyCSS: true,
+        minifyURLs: true,
+        minifyJS: true,
+        removeComments: true,
+        removeRedundantAttributes: true,
+      },
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    new ForkTsCheckerWebpackPlugin(),
+  ],
+};
