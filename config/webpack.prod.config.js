2d48ce3589780d6e4cc47a9bc21d01f9fd235146
Binabh <binabhdevkota@gmail.com>
2020-12-05 19:59:07 +0545

    production build optimization

--- config/webpack.prod.config.js
+++ config/webpack.prod.config.js
@@ -1,15 +1,18 @@
 const path = require('path'),
   webpack = require('webpack'),
   HtmlWebpackPlugin = require('html-webpack-plugin'),
-  ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+  ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin'),
+  MiniCssExtractPlugin = require('mini-css-extract-plugin'),
+  OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin'),
+  TerserPlugin = require('terser-webpack-plugin');
 const APP_PATH = path.resolve(__dirname, '../src');
 
 module.exports = {
   mode: 'production',
   entry: APP_PATH,
   output: {
-    path: path.resolve(__dirname, '../dist', 'js'),
-    filename: '[name].[contenthash].bundle.js',
+    path: path.resolve(__dirname, '../dist'),
+    filename: '[name].[hash].bundle.js',
   },
   devtool: 'source-map',
   devServer: {
@@ -17,17 +20,51 @@ module.exports = {
   },
   resolve: {
     extensions: ['.js', '.json', '.ts', '.tsx'],
+    alias: { src: path.resolve(__dirname, '../src') },
   },
   module: {
     rules: [
       {
-        test: /\.(ts|tsx)$/,
+        test: /\.(ts|tsx|js)$/,
         loader: 'babel-loader',
         exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.s(a|c)ss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]',
+        },
+      },
+      {
+        test: /\.woff(2)?(\?v=[a-z0-9]\.[a-z0-9]\.[a-z0-9])?$/,
+        use: ['url-loader?limit=10000'],
+      },
+      {
+        test: /\.(ttf|eot)(\?v=[a-z0-9]\.[a-z0-9]\.[a-z0-9])?$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]',
+        },
+      },
+      {
+        test: /\.svg$/,
+        use: ['@svgr/webpack', 'url-loader'],
       },
     ],
   },
   plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css',
+    }),
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, '../public', 'index.html'),
       filename: 'index.html',
@@ -42,7 +79,20 @@ module.exports = {
         removeRedundantAttributes: true,
       },
     }),
-    new webpack.HotModuleReplacementPlugin(),
     new ForkTsCheckerWebpackPlugin(),
   ],
+  optimization: {
+    minimizer: [new TerserPlugin({}), new OptimizeCssAssetsPlugin({})],
+    splitChunks: {
+      chunks: 'all',
+    },
+    runtimeChunk: {
+      name: (entrypoint) => `runtimechunk~${entrypoint.name}`,
+    },
+  },
+  performance: {
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000,
+    hints: false,
+  },
 };
